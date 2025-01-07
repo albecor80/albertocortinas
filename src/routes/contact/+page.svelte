d1ef354d0c8d8e7997e6abfbdff3968c2b084bd9
Alberto Cortinas <alberto.cortinas@gmail.com>
2025-01-07 12:33:55 +0100

    reset

--- src/routes/contact/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="mx-auto max-w-xl text-public text-lg">
-    <h1 class=" text-center">How to contact me</h1>
-
-    <p class="mt-4">
-        You can contact me by sending me an e-mail to <a href="mailto:alberto.cortinas@proton.me" class="underline text-pink">by clicking here</a> and I'll be happy to hear from you and discuss your comments or suggenstions
-    </p>
-</div>
-
--- src/routes/projects/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h2 class="text-center">Coming soon</h2>
